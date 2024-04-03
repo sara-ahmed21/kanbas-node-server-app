544d708ba967d2b909bd59252cbd8a1950f5e779
sahmeddd <128559127+sahmeddd@users.noreply.github.com>
2024-04-03 11:52:28 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import express from "express";
+import Lab5 from './Lab5.js';
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+CourseRoutes(app);
+ModuleRoutes(app);
+
+Lab5(app);
+app.listen(process.env.PORT || 4000);
